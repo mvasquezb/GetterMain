e4745624033cc9395e5b3c6f09ae4b4880cadd77
Martín Vásquez <pedromvasquezb@gmail.com>
2017-09-18 19:25:03 +0000

    Add person-user relationship

--- app/Person.php
+++ app/Person.php
@@ -7,6 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Person extends Model
 {
     //
-
     protected $table = 'person';
+
+    public function user()
+    {
+        return $this->hasOne('App\User');
+    }
 }
