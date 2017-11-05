390f7d539891eb854aa180263635e81370a510c1
Martín Vásquez <pedromvasquezb@gmail.com>
2017-11-05 22:22:51 +0000

    Add Store filtering by product category and price

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Store;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class StoreController extends Controller
 {
@@ -14,10 +15,12 @@ class StoreController extends Controller
      */
     public function index(Request $request)
     {
-        $stores = Store::all();
+        $stores = $this->filterStores($request);
+        $stores = $stores->get();
+        
         $withBusinessInfo = $request->input('business_info', false) ?: false;
         if ($withBusinessInfo) {
-            foreach($stores as $store) {
+            foreach ($stores as $store) {
                 $store->append(['business_name', 'business_logo_url']);
             }
         }
@@ -103,5 +106,54 @@ class StoreController extends Controller
     public function destroy(Store $store)
     {
         //
+    }
+
+    private function filterStores($request)
+    {
+        $filterCategories = $request->category;
+        $filterPrices = $request->price;
+
+        $query = Store::query();
+        if ($filterCategories || $filterPrices) {
+            $query = $query->join('offers', 'offers.store_id', '=', 'stores.id')
+                    ->join('products', 'products.id', '=', 'offers.product_id');
+        }
+
+        if ($filterCategories) {
+            $query = $query->join('product_categories', 'product_categories.id', '=', 'products.category_id')
+                    ->whereIn('product_categories.slug', $filterCategories);
+        }
+        
+        if ($filterPrices) {
+            $prices = $this->cleanPriceFilters($filterPrices);
+            foreach ($prices as $price) {
+                $query = $query->where('products.price', $price['restriction'], $price['amount']);
+            }
+        }
+        return $query;
+    }
+
+    private function cleanPriceFilters($priceFilters)
+    {
+        $prices = [];
+        foreach ($priceFilters as $filter) {
+            $restriction = substr($filter, 0, 1);
+            $amount = substr($filter, 1);
+            if (!in_array($restriction, ['<', '=', '>'])) {
+                continue;
+            }
+            if (!is_numeric($amount)) {
+                continue;
+            }
+            $amount = floatval($amount);
+            if ($amount <= 0) {
+                continue;
+            }
+            $prices[] = [
+                'restriction' => $restriction,
+                'amount' => $amount,
+            ];
+        }
+        return $prices;
     }
 }
